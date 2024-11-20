bb115c7ecfeb309d20194ddb354312b3e40cc240
Fahim_Siam <siddiquifahimga1@gmail.com>
2024-11-20 16:24:51 +0600

    corrected main

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,18 @@ public class Main {
             IceCreamShop shop = new IceCreamShop();
             Order order = new Order(shop);
 
-            // Add flavors
             order.addFlavor("Chocolate Fudge", 1);
             order.addFlavor("Mint Chocolate Chip", 1);
             order.addFlavor(" Pistachio Delight", 1);
 
 
-            // Add toppings
             order.addTopping("Chocolate Chips", 1);
             order.addTopping("Fresh Strawberries", 2);
 
-            // Set container
             order.setWaffleCone(true);
 
-            // Print the total
             System.out.println("Total Amount Due: $" + String.format("%.2f", order.calculateTotal()));
 
-            // Generate an invoice
             order.generateInvoice("invoice.txt");
             System.out.println("Invoice generated as 'invoice.txt'.");
         } catch (Exception e) {
